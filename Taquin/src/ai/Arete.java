5b100615e2056028b0ec6e450cfd055800194831
Seb <deprez.seba@gmail.com>
2013-02-22 15:37:23 +0100

    Ajout de l'AI

---
+++ Taquin/src/ai/Arete.java
@@ -0,0 +1,18 @@
+package ai;
+
+public class Arete {
+	
+	// configuration de depart
+	public Taquin origine;
+	// configuration d'arrivee
+	public Taquin destination;
+	// stocke le caractere correspondant au mouvement
+	public char mvmt;
+	
+	public Arete(Taquin _origine, Taquin _destination, char _mvmt) {
+		origine = _origine;
+		destination = _destination;
+		mvmt = _mvmt;
+	}
+	
+}
